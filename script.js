f8572934b751847c766e9b6e960046a35f1fad6e
Ricardo Hoyos <rimaylu@gmail.com>
2022-11-08 01:39:43 -0600

    modificando index

---
+++ script.js
@@ -0,0 +1,47 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+let navbar = document.querySelector('.header .navbar');
+let searchForm = document.querySelector('.header .search-form');
+let loginForm = document.querySelector('.header .login-form');
+let contactInfo = document.querySelector('.contact-info');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   searchForm.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+document.querySelector('#search-btn').onclick = () =>{
+   searchForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   loginForm.classList.remove('active');
+};
+
+document.querySelector('#login-btn').onclick = () =>{
+   loginForm.classList.toggle('active');
+   navbar.classList.remove('active');
+   searchForm.classList.remove('active'); 
+};
+
+document.querySelector('#info-btn').onclick = () =>{
+   contactInfo.classList.add('active');
+}
+
+document.querySelector('#close-contact-info').onclick = () =>{
+   contactInfo.classList.remove('active');
+}
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click',()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
